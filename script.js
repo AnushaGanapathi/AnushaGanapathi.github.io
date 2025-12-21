7482cbe51837b37fa1c552a0bc1d495417d595d1
Anusha <anushaganapathi@outlook.com>
2025-12-21 22:28:04 +0530

    design change

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log("script.js loaded");
+
+function openPopup() {
+  document.getElementById("overlay").style.display = "block";
+  document.getElementById("errorMsg").style.display = "none";
+  document.getElementById("passwordInput").value = "";
+}
+
+function closePopup() {
+  document.getElementById("overlay").style.display = "none";
+}
+
+function submitPassword() {
+  var pwd = document.getElementById("passwordInput").value;
+
+  if (pwd === "gandalf") {
+    window.location.href = "cv.html";
+  } else {
+    document.getElementById("errorMsg").style.display = "block";
+  }
+}
